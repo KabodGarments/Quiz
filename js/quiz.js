cd954954cbdb60c5f2405087cb4b94daf70deb79
KabodGarments <kabodgarments@gmail.com>
2019-02-21 12:14:17 +0300

    made some js and css edits on the branch as an experiment of simplifying things. Later to be merged with master if it works well

--- js/quiz.js
+++ js/quiz.js
@@ -6,8 +6,6 @@ const qImg = document.getElementById("qImg");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 
