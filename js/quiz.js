824ecdae052c5a3f09ee05c62a09f79e2a4aea72
KabodGarments <kabodgarments@gmail.com>
2019-02-25 09:45:32 +0300

    js edits and css styling edits

--- js/quiz.js
+++ js/quiz.js
@@ -2,7 +2,6 @@
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
